d6c90f98a3a326ab8e4a71c53b9ac93c3c858825
platane <me@platane.me>
2020-02-11 16:22:00 +1100

    :notebook: add badges, rename the package

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 ![type definitions](https://img.shields.io/npm/types/typescript?style=flat-square)
 ![code style](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)
+[![npm](https://img.shields.io/npm/v/github-secret-dotenv?style=flat-square)](https://www.npmjs.com/package/github-secret-dotenv)
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/Platane/github-secret-dotenv/main?style=flat-square)](https://github.com/Platane/github-secret-dotenv/actions?query=workflow%3Amain)
+
+> Upload secrets in github from your .env file
+
+# Usage
+
+```
+github-secret --repository platane/github-secret-dotenv --path ./.env --githubAccessToken xxxx
+```
+
+> if `repository` is omitted, the github repository is read from the package.json
+
+> `githubAccessToken` can be passed as env var `GITHUB_ACCESS_TOKEN`
--- src/__tests__/readPackageJson.spec.ts
+++ src/__tests__/readPackageJson.spec.ts
@@ -4,29 +4,29 @@ import path from "path";
 it("parse from repository.url", async () => {
   expect(
     parsePackageJson({
-      repository: { url: "https://github.com/platane/github-secret-manager" }
+      repository: { url: "https://github.com/platane/github-secret-dotenv" }
     })
   ).toEqual({
     owner: "platane",
-    repo: "github-secret-manager"
+    repo: "github-secret-dotenv"
   });
 });
 
 it("parse from author and name", async () => {
   expect(
     parsePackageJson({
-      name: "github-secret-manager",
+      name: "github-secret-dotenv",
       author: { name: "platane" }
     })
   ).toEqual({
     owner: "platane",
-    repo: "github-secret-manager"
+    repo: "github-secret-dotenv"
   });
 });
 
 it("readPackageJson", async () => {
   expect(readPackageJson(path.resolve(__dirname, "../.."))).toEqual({
     owner: "platane",
-    repo: "github-secret-manager"
+    repo: "github-secret-dotenv"
   });
 });
