d53d0dd6338a396e19e295d4a32156a17234bf09
platane <me@platane.me>
2020-03-17 10:08:16 -0700

    :rocket: add test to ensure the env var in context are not used

--- src/__tests__/dotEnv.spec.ts
+++ src/__tests__/dotEnv.spec.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import { readEnv } from "../dotEnv";
 
-it("read simple .env", async () => {
+it("should read simple .env", async () => {
   const env = readEnv({
     path: path.resolve(__dirname, "..", "__fixtures__", ".env.simple")
   });
@@ -9,9 +9,17 @@ it("read simple .env", async () => {
   expect(env).toContainEqual({ name: "XXX_TEST", value: "0000000000" });
 });
 
+it("should not interpolate with context env var", async () => {
+  const env = readEnv({
+    path: path.resolve(__dirname, "..", "__fixtures__", ".env.simple")
+  });
+
+  expect(env).toContainEqual({ name: "LEAK_TEST", value: "0000000000" });
+});
+
 it("should expand dotenv vars", () => {
   const env = readEnv({
     path: path.resolve(__dirname, "..", "__fixtures__", ".env.simple")
   });
-  expect(env).toContainEqual({ name: "ZZZ_TEST", value: "00000000001"});
+  expect(env).toContainEqual({ name: "ZZZ_TEST", value: "00000000001" });
 });
